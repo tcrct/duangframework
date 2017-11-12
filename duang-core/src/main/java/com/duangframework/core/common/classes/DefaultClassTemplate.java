ff3584e550c23908bec2d35fd0eaa38982fcf96d
laotang <tcrct@163.com>
2017-11-12 21:48:54 +0800

    添加类扫描

--- duang-core/src/main/java/com/duangframework/core/common/classes/DefaultClassTemplate.java
+++ duang-core/src/main/java/com/duangframework/core/common/classes/DefaultClassTemplate.java
@@ -1,11 +1,27 @@
-//package com.duangframework.core.common.classes;
-//
-///**
-// * Created by laotang on 2017/11/12 0012.
-// */
-//public class DefaultClassTemplate extends AbstractClassTemplate {
-//
-//    public DefaultClassTemplate(Map<>) {
-//
-//    }
-//}
+package com.duangframework.core.common.classes;
+
+import com.duangframework.core.annotation.mvc.Controller;
+import com.duangframework.core.annotation.mvc.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by laotang on 2017/11/12 0012.
+ */
+public class DefaultClassTemplate extends AbstractClassTemplate {
+
+    private static Map<String, Class<?>> annotationMap = new HashMap<>();
+    static {
+        annotationMap.put(Controller.class.getName(), Controller.class);
+        annotationMap.put(Service.class.getName(), Service.class);
+    }
+
+    public DefaultClassTemplate() {
+        super(annotationMap);
+    }
+
+    public DefaultClassTemplate(Map<String, Class<?>> annotationMap) {
+        super(annotationMap);
+    }
+}
