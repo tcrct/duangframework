e305f8c4073cb1df7033617d6809239c53ab37b2
tcrct@163.com <tcrct790731>
2017-12-12 23:41:08 +0800

    解决server模块对mvc模块类依赖问题

--- duang-rpc/src/main/java/com/duangframework/rpc/plugin/RpcPlugin.java
+++ duang-rpc/src/main/java/com/duangframework/rpc/plugin/RpcPlugin.java
@@ -1,14 +1,34 @@
 package com.duangframework.rpc.plugin;
 
 import com.duangframework.core.interfaces.IPlugin;
+import com.duangframework.core.kit.ConfigKit;
+import com.duangframework.core.kit.ThreadPoolKit;
+import com.duangframework.rpc.server.RpcServer;
+import jdk.nashorn.internal.runtime.regexp.joni.Config;
 
 /**
  * @author Created by laotang
  * @date on 2017/12/6.
  */
 public class RpcPlugin implements IPlugin {
+
+    private static RpcServer rpcServer;
+
     @Override
     public void init() throws Exception {
+        ThreadPoolKit.execute(new Runnable() {
+            @Override
+            public void run() {
+                String host = ConfigKit.duang().key("rpc.host").defaultValue("0.0.0.0").asString();
+                int port = ConfigKit.duang().key("rpc.port").defaultValue(9091).asInt();
+                rpcServer = new RpcServer(host, port);
+                try {
+                    rpcServer.start();
+                } catch (Exception e) {
+                    rpcServer.shutdown();
+                }
+            }
+        });
 
     }
 
@@ -19,6 +39,6 @@ public class RpcPlugin implements IPlugin {
 
     @Override
     public void stop() throws Exception {
-
+        rpcServer.shutdown();
     }
 }
